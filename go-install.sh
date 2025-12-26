739f18df0de7d56b246a7ab774d93d507b82319a
Ilya Z <ilya.zuyev@gmail.com>
2025-12-26 15:17:46 -0800

    add air

--- go-install.sh
+++ go-install.sh
@@ -1,3 +1,4 @@
  go install github.com/karol-broda/snitch@latest
  go install github.com/wader/fq@latest
  go install github.com/jesseduffield/lazygit@latest
+ go install github.com/air-verse/air@latest
