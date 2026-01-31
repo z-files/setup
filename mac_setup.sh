fddb686bafd6d6cb40d199c7c8dbf1b27487513c
Ilya Z <ilyaz@revol.local>
2026-01-30 16:19:12 -0800

    add mac setup script :

---
+++ mac_setup.sh
@@ -0,0 +1,17 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+brew install sdkman curl -s "https://get.sdkman.io" | bash
+curl -s "https://get.sdkman.io" | bash
+
+brew install git
+brew install tmux
+brew install nvim
+brew install kind
+brew install kubectl
+brew install python3
+brew install awsume
+brew install sdkman curl -s "https://get.sdkman.io" | bash
+brew install htop
+brew install k9s
+brew install teleport
+brew install tig
